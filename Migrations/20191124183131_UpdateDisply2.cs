50a49011075ff7739fb148b7f8cd08fc1292f030
github <kygibbs@umich.edu>
2019-11-24 13:38:07 -0500

    Changed "CreatedDate" column header to "Created". Changed Cost Datatype from flow to decimal. Fixed leftover junk text from tutorials.

---
+++ Migrations/20191124183131_UpdateDisply2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeImprovement3._0.Migrations
+{
+    public partial class UpdateDisply2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedByID",
+                table: "Project",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByID",
+                table: "Project");
+        }
+    }
+}
--- Models/Project.cs
+++ Models/Project.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace HomeImprovement3._0.Models
 {
     public class Project
     {
         public int Id { get; set; }
+
+        public int CreatedByID { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
 
+        [Display(Name = "Created")]
         [DataType(DataType.Date)]
         public DateTime CreatedDate { get; set; }
-        public int Cost { get; set; }
+        
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Cost { get; set; }
     }
 }
